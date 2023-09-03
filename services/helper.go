0caf0d52fd2c5d16d239da581ec70e1860b1ce9f
nonokaM <c1201071@gmail.com>
2023-09-03 20:44:23 +0900

    add: api完成

--- services/helper.go
+++ services/helper.go
@@ -3,16 +3,23 @@ package services
 import (
 	"database/sql"
 	"fmt"
-	"os"
+	// "os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	dbUser     = os.Getenv("DB_USER")
-	dbPassword = os.Getenv("DB_PASSWORD")
-	dbDatabase = os.Getenv("DB_NAME")
+	// ハンズオン通りだと読み込みがうまくいかなかったため、とりあえず直書き
+	// dbUser     = os.Getenv("DB_USER")
+	// dbPassword = os.Getenv("DB_PASSWORD")
+	// dbDatabase = os.Getenv("DB_NAME")
+	// dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	dbUser     = "docker"
+	dbPassword = "docker"
+	dbDatabase = "sampledb"
 	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
 )
 
 func connectDB() (*sql.DB, error) {
